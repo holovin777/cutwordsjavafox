af149d2c124e12eb4c4190bf1a0fa24517d59311
Viktor Holovin <holovin@mail.com>
2022-07-27 22:31:10 +0200

    For loop

--- src/org/cutwordsjavafox/WordCuter.java
+++ src/org/cutwordsjavafox/WordCuter.java
@@ -4,26 +4,13 @@ public class WordCuter {
 
 	public static void main(String[] args) {
 
-		String source = "Hello, world!";
+		String source = "Hello, world! My name is";
 		String[] words = source.split(" ");
-		System.out.println(words[0]);
-		System.out.println(words[1]);
-		char[] letters1 = words[0].toCharArray();
-		char[] letters2 = words[1].toCharArray();
-		System.out.println(letters1.length);
-		System.out.println(letters2.length);
-		String[][] words2d = {{words[0]}, {words[1]}};
-		System.out.println(words2d.length);
-		System.out.println(words2d[0][0]);
-		System.out.println(words2d[1][0]);
 
-		System.out.println("- Exercises");
 		for (int i = 0; i < words.length; i++) {
-			System.out.println(words[i]);
 			char[] letters = words[i].toCharArray();
-			for (int u = 0; u < letters.length; u++) {
-				System.out.println(letters[u]);
-			}
+			System.out.println("There are " + letters.length + " letters in the word: " + "\"" + words[i] + "\"");
+
 		}
 
 	}
