ed678b9e112d42fc6119cfd809554f17d17158c4
Viktor Holovin <holovin@mail.com>
2022-07-28 16:47:01 +0200

    Loop lesson

--- src/org/cutwordsjavafox/WordCuter.java
+++ src/org/cutwordsjavafox/WordCuter.java
@@ -6,12 +6,22 @@ public class WordCuter {
 
 		String source = "Hello, world! My name is";
 		String[] words = source.split(" ");
+		StringBuilder recoverSource = new StringBuilder();
 
 		for (int i = 0; i < words.length; i++) {
 			char[] letters = words[i].toCharArray();
+
+			for (int j = 0; j < letters.length; j++) {
+				recoverSource.append(letters[j]);
+			}
+
+			recoverSource.append(" ");
+
+			System.out.println(recoverSource);
 			System.out.println("There are " + letters.length + " letters in the word: " + "\"" + words[i] + "\"");
 
 		}
+
 
 	}
 
