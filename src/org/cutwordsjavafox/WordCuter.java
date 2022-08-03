770b08245f7fa94687d64f4679582599a656c2d1
Viktor Holovin <holovin@mail.com>
2022-08-04 00:17:38 +0200

    Ex. with ArrayList

--- src/org/cutwordsjavafox/WordCuter.java
+++ src/org/cutwordsjavafox/WordCuter.java
@@ -1,17 +1,22 @@
 package org.cutwordsjavafox;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class WordCuter {
 
 	public static void main(String[] args) {
 
 		String source = "Hello, world! My name is";
-		String[] words = source.split(" ");
+		String[] wordsAfterSplit = source.split(" ");
+		List<String> words = new ArrayList<>(Arrays.asList(wordsAfterSplit));
 		StringBuilder recoverSource = new StringBuilder();
 
-		for (int i = 0; i < words.length; i++) {
-			char[] letters = words[i].toCharArray();
+		for (int i = 0; i < words.size(); i++) {
+			List<String> letters = new ArrayList<>();
+			letters.add(words[i]);
 
-			for (int j = 0; j < letters.length; j++) {
+			for (int j = 0; j < letters.size(); j++) {
 				recoverSource.append(letters[j]);
 			}
 
